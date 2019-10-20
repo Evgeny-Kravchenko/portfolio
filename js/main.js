fabcf5d64141e4ab5dd26935cb1e816b4f50d404
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2019-10-20 18:43:06 +0300

    feat: add project navigation

--- js/main.js
+++ js/main.js
@@ -154,7 +154,7 @@ const swipeDetect = (el) => {
   let restraint = 150;
   //Длительность swipe
   let allowedTime = 1000;
-  
+
   //Событие для touch
   surface.addEventListener("touchstart", function(e) {
     if(e.target.classList.contains("btn-left")) {
@@ -204,6 +204,129 @@ const swipeDetect = (el) => {
 
 let el = document.querySelector("#slide");
 swipeDetect(el);
+
+//Go to project
+
+const projectShow = function() {
+
+  //Переменные для хранения координат touchstart и touchend
+  let startX = 0;
+  let startY = 0;
+  let endX = 0;
+  let endY = 0;
+  let page = document.querySelector("#wrapper");
+
+  //THEYALOW
+
+  let theyalow = document.querySelector("#theyalow");
+  let theyalowIframe = document.querySelector("#theyalow-iframe");
+  let switchTheyalow = document.querySelector("#switch-theyalow");
+  let backTheyalow  = document.querySelector("#back-theyalow");
+
+  //Клик по проекту theyalow
+  theyalow.addEventListener("click", function() {
+    page.style.display = "none";
+    theyalowIframe.style.display = "block";
+    switchTheyalow.style.display = "block";
+    backTheyalow.style.display = "block";
+  })
+
+  //Клик по кнопке переключения Desktop/Mobile
+  switchTheyalow.addEventListener("click", function(e) {
+    theyalowIframe.classList.toggle("mobile-theyalow");
+    if(switchTheyalow.innerHTML === "Mobile") {
+      switchTheyalow.innerHTML = "Desktop";
+    } else {
+      switchTheyalow.innerHTML = "Mobile";
+    }
+  })
+
+  //Клик по кнопке back
+  backTheyalow.addEventListener("click", function() {
+    page.style.display = "block";
+    theyalowIframe.style.display = "none";
+    switchTheyalow.style.display = "none";
+    backTheyalow.style.display = "none";
+  })
+
+  //Touch по проекту theyalow
+  theyalow.addEventListener("touchstart", function(e) {
+    let touchObj = e.changedTouches[0];
+    startX = touchObj.pageX;
+    startY = touchObj.pageY;
+  })
+
+  theyalow.addEventListener("touchend", function(e) {
+    let touchObj = e.changedTouches[0];
+    endX = touchObj.pageX;
+    endY = touchObj.pageY;
+
+    if(startX === endX && startY === endY) {
+      page.style.display = "none";
+      theyalowIframe.style.display = "block";
+      backTheyalow.style.display = "block";
+      backTheyalow.style.right = "20px";
+    }
+  })
+
+
+
+  //REPAIR-DESIGN-PROJECT
+  let repair = document.querySelector("#repair-design-project");
+  let repairIframe = document.querySelector("#repair-iframe");
+  let switchRepair = document.querySelector("#switch-repair");
+  let backRepair = document.querySelector("#back-repair");
+
+  //Клик по проекту repair-design-project
+  repair.addEventListener("click", function() {
+    page.style.display = "none";
+    repairIframe.style.display = "block";
+    switchRepair.style.display = "block";
+    backRepair.style.display = "block";
+  })
+
+  //Клик по кнопке переключения Desktop/Mobile
+  switchRepair.addEventListener("click", function(e) {
+    repairIframe.classList.toggle("mobile-repair");
+    if(switchTheyalow.innerHTML === "Mobile") {
+      switchRepair.innerHTML = "Desktop";
+    } else {
+      switchRepair.innerHTML = "Mobile";
+    }
+  })
+
+  //Клик по кнопке back
+  backRepair.addEventListener("click", function() {
+    page.style.display = "block";
+    repairIframe.style.display = "none";
+    switchRepair.style.display = "none";
+    backRepair.style.display = "none";
+  })
+
+  //Touch по проекту theyalow
+  repair.addEventListener("touchstart", function(e) {
+    let touchObj = e.changedTouches[0];
+    startX = touchObj.pageX;
+    startY = touchObj.pageY;
+  })
+
+  repair.addEventListener("touchend", function(e) {
+    let touchObj = e.changedTouches[0];
+    endX = touchObj.pageX;
+    endY = touchObj.pageY;
+
+    if(startX === endX && startY === endY) {
+      page.style.display = "none";
+      repairIframe.style.display = "block";
+      backRepair.style.display = "block";
+      backRepair.style.right = "20px";
+    }
+  })
+}
+
+projectShow();
+
+
 
 
 
