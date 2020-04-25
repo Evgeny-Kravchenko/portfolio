32e1510bed3b8a5fd3fb358a24113a2007a58171
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2020-04-25 18:43:12 +0300

    feat: add core module feat: add header component feat: add home-page component feat: add the necessary fonts to the project

---
+++ portfolio/src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, HomePageComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [ HeaderComponent ]
+})
+export class CoreModule { }
