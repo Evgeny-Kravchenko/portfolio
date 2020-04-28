a16d78183f8f8b7aeeddb5b08eadf925e249b28e
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2020-04-28 17:25:22 +0300

    feat: add the description component

--- portfolio/src/app/core/core.module.ts
+++ portfolio/src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HammerModule } from '@angular/platform-browser';
+import { DescriptionComponent } from './components/description/description.component';
 
 @NgModule({
-  declarations: [HeaderComponent, HomePageComponent],
+  declarations: [HeaderComponent, HomePageComponent, DescriptionComponent],
   imports: [CommonModule, HammerModule],
   exports: [HeaderComponent, HammerModule],
 })
