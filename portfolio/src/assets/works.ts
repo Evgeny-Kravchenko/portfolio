1001579010deebafdf5d69d9bc0ea9521266195c
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2020-05-02 13:46:49 +0300

    fix: change hover animation feat: add a application description

--- portfolio/src/assets/works.ts
+++ portfolio/src/assets/works.ts
@@ -16,36 +16,36 @@ export const works: Array<IWorkItem> = [
     type: 'HTML&CSS',
   },
   {
-    title: 'Culture portal',
-    imageSrc: './assets/images/projects-image/culture-project.jpg',
+    title: 'Fancy weather',
+    imageSrc: './assets/images/projects-image/weather.jpg',
+    description: 'This is a weather app. The following technologies were used to implement the application: HTML, CSS, Java script. The architecture of this application is based on the principles of MVC (Model View Controller). This application also used APIs that were provided by different services.',
+    type: 'JS',
+  },
+  {
+    title: 'Piskel clone',
+    imageSrc: './assets/images/projects-image/piskel-clone.png',
     description:
-      'This project supports responsive web design. Resizing the screen does not spoil the page design. This page supports 3 browsers: Google Chrome, Mozilla Firefox, Microsoft Edge. Here was used the toolkit for automating painful or time-consuming tasks "Gulp" and preprocessor "Sass".',
+      'This is an animation editor application. Here you can create an pixel animation and save it on the device. This is my first app where I tried to use Angular. This is the final work from RS 2019 Q3 course.',
     type: 'Angular',
   },
   {
     title: 'Youtube client',
     imageSrc: './assets/images/projects-image/youtube-client.png',
     description:
-      'This project supports responsive web design. Resizing the screen does not spoil the page design. This page supports 3 browsers: Google Chrome, Mozilla Firefox, Microsoft Edge. Here was used the toolkit for automating painful or time-consuming tasks "Gulp" and preprocessor "Sass".',
+      'This is a YouTube client application where you can search for information about any video on YouTube. This application uses many of the Angular capabilities such as modules, components, pipes, decorators, interceptors and guards.',
     type: 'Angular',
   },
   {
-    title: 'Piskel clone',
-    imageSrc: './assets/images/projects-image/piskel-clone.png',
+    title: 'Culture portal',
+    imageSrc: './assets/images/projects-image/culture-project.jpg',
     description:
-      'This project supports responsive web design. Resizing the screen does not spoil the page design. This page supports 3 browsers: Google Chrome, Mozilla Firefox, Microsoft Edge. Here was used the toolkit for automating painful or time-consuming tasks "Gulp" and preprocessor "Sass".',
+      'This is a final work from RSSchool Angular 2020 Q1 course. You can find information about some Belarusian directors on this portal. It was a team work where the team and I used the GitHub project to organize works and split tasks between team members.',
     type: 'Angular',
-  },
-  {
-    title: 'Fancy weather',
-    imageSrc: './assets/images/projects-image/weather.jpg',
-    description: '',
-    type: 'JS',
   },
   {
     title: 'NodeJS',
     imageSrc: './assets/images/projects-image/nodeJS.jpg',
-    description: '',
+    description: 'This is the result of my work at RSSchool NodeJS 2020 Q1. This is an API leisure application where I used technologies like express, mongoose and mongoDB. This application works on the principle of CRUD (Create Read Update Delete). 3-layer architecture was used in this app.',
     type: 'NodeJS',
   },
 ];
