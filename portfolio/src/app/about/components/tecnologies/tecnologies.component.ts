810fd3e7032dff0710d22b435625f49a039e3902
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2020-08-03 23:19:05 +0300

    Auto-generated commit

--- portfolio/src/app/about/components/tecnologies/tecnologies.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-tecnologies',
-  templateUrl: './tecnologies.component.html',
-  styleUrls: ['./tecnologies.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class TecnologiesComponent {}
